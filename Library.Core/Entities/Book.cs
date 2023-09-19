e23729b1bd7ee1b549b6070df57e0f7240fb92d7
Mobin <mobinkolyaei@gmail.com>
2023-09-19 13:08:45 +0330

    Add Book entity and IReadable interface

---
+++ Library.Core/Entities/Book.cs
@@ -0,0 +1,30 @@
+﻿using Library.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.Entities
+{
+    public class Book : BaseEntity , IReadable
+    {
+        public string? Title { get; set; }
+        public string? Writer { get; set; }
+        public double Price { get; set; }
+
+        public void Read()
+        {
+            Console.Write("Title: ");
+            Title = Console.ReadLine();
+            Console.Write("Writer: ");
+            Writer = Console.ReadLine();
+            Console.Write("Price: ");
+            Price = double.Parse(Console.ReadLine()??"0");
+        }
+        public override string ToString()
+        {
+            return $"Book : {Id} {Title} {Writer} {Price} {CreationTime}";
+        }
+    }
+}
