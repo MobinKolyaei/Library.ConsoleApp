358f70b7f0491bd6fcd99fccf9d625aaa08072c5
Mobin <mobinkolyaei@gmail.com>
2023-09-19 13:31:35 +0330

    Add Sqlserver package and make tables

---
+++ Library.Infrastructure/Data/LibraryDB.cs
@@ -0,0 +1,22 @@
+﻿using Library.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Infrastructure.Data
+{
+    public class LibraryDB : DbContext 
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Borrow> Borrows { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocaldb;Database=LDB;Trusted_Connection=True;");
+        }
+    }
+}
