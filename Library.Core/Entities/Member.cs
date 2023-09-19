380e733cda01d36ec65cbffe66b6455d4a756a16
Mobin <mobinkolyaei@gmail.com>
2023-09-19 13:11:57 +0330

    Add Member entity

---
+++ Library.Core/Entities/Member.cs
@@ -0,0 +1,26 @@
+﻿using Library.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.Entities
+{
+    public class Member : BaseEntity, IReadable
+    {
+        public string? Fullname { get; set; }
+        public string? Address { get; set; }
+        public void Read()
+        {
+            Console.Write("Fullname: ");
+            Fullname = Console.ReadLine();
+            Console.Write("Address: ");
+            Address = Console.ReadLine();
+        }
+        public override string ToString()
+        {
+            return $"Member : {Id} {Fullname} {Address} {CreationTime}";
+        }
+    }
+}
